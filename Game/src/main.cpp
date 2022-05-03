1efb28b6fc4d899b9a7ef9e290e39bdfc3808ff5
pichi02 <lucas.pich02@gmail.com>
2022-05-02 21:10:23 -0300

    Added pichlib

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -2,40 +2,43 @@
 #include "CaveLib.h"
 #include "RamiLib.h"
 #include "FranLib.h"
+#include"pichlib.h"
 
 void Run()
 {
-    // Initialization (Note windowTitle is unused on Android)
-    //---------------------------------------------------------
-    InitWindow(300, 300, "Legendary Lamp");
-    
-    //gameIcon = LoadImage(gameIconUrl);
-    //SetWindowIcon(gameIcon);
+	// Initialization (Note windowTitle is unused on Android)
+	//---------------------------------------------------------
+	InitWindow(300, 300, "Legendary Lamp");
 
-    //InitGame();
+	//gameIcon = LoadImage(gameIconUrl);
+	//SetWindowIcon(gameIcon);
 
-    SetTargetFPS(60);
-    //--------------------------------------------------------------------------------------
+	//InitGame();
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update and Draw
-        //----------------------------------------------------------------------------------
-        //UpdateDrawGameFrame();
-        //----------------------------------------------------------------------------------
+	SetTargetFPS(60);
+	//--------------------------------------------------------------------------------------
+	Texture2D texture = loadTexture("bojji.png");
+	// Main game loop
+	while (!WindowShouldClose())    // Detect window close button or ESC key
+	{
+		BeginDrawing();
+		ClearBackground(BLACK);
+		// Update and Draw
+		//----------------------------------------------------------------------------------
+		//UpdateDrawGameFrame();
+		//----------------------------------------------------------------------------------
+		EndDrawing();
+	}
 
-    }
+	// De-Initialization
+	//--------------------------------------------------------------------------------------
+	//UnloadGame();         // Unload loaded data (textures, sounds, models...)
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    //UnloadGame();         // Unload loaded data (textures, sounds, models...)
-
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+	CloseWindow();        // Close window and OpenGL context
+	//--------------------------------------------------------------------------------------
 }
 
 void main()
 {
-    Run();
+	Run();
 }
---
+++ libs/pichlib/include/pichlib.h
@@ -0,0 +1,4 @@
+#pragma once
+
+Texture2D loadTexture(const char* name);
+void drawImage(Texture2D texture, int x, int y);
